a2de18d5cb47015fc1f87573287e0e93cacf1f5a
Max <ewheel17@gmail.com>
2018-04-27 17:50:38 -0600

    Added Proof of Work functionality.

--- main.js
+++ main.js
@@ -7,16 +7,29 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log('Block mined: ' + this.hash);
     }
 }
 
+// Creates a BlockChain. Difficulty of Proof of Work can be altered 
+// by changing the this.difficulty value in the constructor.
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
     }
 
     createGenesisBlock() {
@@ -29,7 +42,7 @@ class Blockchain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -56,15 +69,10 @@ class Blockchain {
 
 
 var maxCoin = new Blockchain();
+
+console.log('Mining block 1...');
 maxCoin.addBlock(new Block(1, "10/07/2018", {amount: 4}));
+
+console.log('Mining block 2...');
 maxCoin.addBlock(new Block(2, "12/07/2018", {amount: 10}));
 
-console.log('Is blockchain valid? ' + maxCoin.isChainValid());
-
-// Try tampering with one of the blocks and rechecking validity.
-maxCoin.chain[1].data = {amount: 100};
-maxCoin.chain[1].hash = maxCoin.chain[1].calculateHash();
-
-console.log('Is blockchain valid? ' + maxCoin.isChainValid());
-
-//console.log(JSON.stringify(maxCoin, null, 4));
